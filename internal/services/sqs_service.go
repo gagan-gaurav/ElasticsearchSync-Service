9e467607f10807fa4d34a10cd77a6339a63323e6
Gagan Gaurav <iamgagangaurav@gmail.com>
2023-08-18 03:05:49 +0530

    sqs service created to push document data to sqs queue.

---
+++ internal/services/sqs_service.go
@@ -0,0 +1,52 @@
+package services
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"fold/internal/models"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+)
+
+func SQS(doc *models.DenormalizedProject) error {
+	// Load AWS configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		fmt.Println("Error loading AWS config:", err)
+		return err
+	}
+
+	// Create an SQS client
+	client := sqs.NewFromConfig(cfg)
+
+	queueURL := "https://sqs.ap-south-1.amazonaws.com/182059941907/syncElasticsearch.fifo"
+
+	jsonBytes, err := json.Marshal(doc)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return err
+	}
+
+	// Create a new message
+	messageGroupId := "group-elastic"       // Replace with your desired message group ID
+	messageDeduplicationId := "unique-id-1" // Replace with your desired deduplication ID
+
+	// Send message to the SQS FIFO queue
+	sendMessageInput := &sqs.SendMessageInput{
+		QueueUrl:               aws.String(queueURL),
+		MessageBody:            aws.String(string(jsonBytes)),
+		MessageGroupId:         aws.String(messageGroupId),
+		MessageDeduplicationId: aws.String(messageDeduplicationId),
+	}
+	_, err = client.SendMessage(context.TODO(), sendMessageInput)
+	if err != nil {
+		fmt.Println("Error sending message:", err)
+		return err
+	}
+
+	fmt.Println("Message sent to SQS successfully!")
+	return nil
+}
