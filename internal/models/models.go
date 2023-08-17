3ea24f42bf820b2a08e1ceeeb0d5cb35a9e6310e
Gagan Gaurav <iamgagangaurav@gmail.com>
2023-08-18 02:48:53 +0530

    models.go added to represent the entities structure.

---
+++ internal/models/models.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"time"
+)
+
+// Define a struct for your entities
+type User struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Hashtag struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Project struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Slug        string    `json:"slug"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UserIds     []int     `json:"user_ids"`
+	HashtagIds  []int     `json:"hashtag_ids"`
+}
+
+type DenormalizedProject struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Slug        string    `json:"slug"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	Users       []User    `json:"user"`
+	Hashtags    []Hashtag `json:"hashtag"`
+}
