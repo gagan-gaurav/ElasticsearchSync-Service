4abc283d5270198d2901c201a0606ca350cd6912
Gagan Gaurav <iamgagangaurav@gmail.com>
2023-08-19 11:10:33 +0530

    created new routes to support CRUD

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -12,12 +12,18 @@ func SetRouter() {
 	r := mux.NewRouter()
 
 	// Define routes
-	r.HandleFunc("/users", handlers.CreateUser).Methods("POST")
-	r.HandleFunc("/users/{id}", handlers.GetUser).Methods("GET")
-	r.HandleFunc("/hashtags", handlers.CreateHashtag).Methods("POST")
-	r.HandleFunc("/hashtags/{id}", handlers.GetHashtag).Methods("GET")
-	r.HandleFunc("/projects", handlers.CreateProject).Methods("POST")
-	r.HandleFunc("/projects/{id}", handlers.GetProject).Methods("GET")
+	r.HandleFunc("/users", handlers.CreateUser).Methods("POST")                     // Create user
+	r.HandleFunc("/users/{id}", handlers.GetUser).Methods("GET")                    // Get user
+	r.HandleFunc("/users/update/{id}", handlers.UpdateUser).Methods("POST")         // Update user
+	r.HandleFunc("/users/delete/{id}", handlers.DeleteUser).Methods("DELETE")       // Delete user
+	r.HandleFunc("/hashtags", handlers.CreateHashtag).Methods("POST")               // Create hashtag
+	r.HandleFunc("/hashtags/{id}", handlers.GetHashtag).Methods("GET")              // Get hashtag
+	r.HandleFunc("/hashtags/update/{id}", handlers.UpdateHashtag).Methods("POST")   // Update hashtag
+	r.HandleFunc("/hashtags/delete/{id}", handlers.DeleteHashtag).Methods("DELETE") // Delete hastag
+	r.HandleFunc("/projects", handlers.CreateProject).Methods("POST")               // Create project
+	r.HandleFunc("/projects/{id}", handlers.GetProject).Methods("GET")              // Get project
+	r.HandleFunc("/projects/update/{id}", handlers.UpdateProject).Methods("POST")   // Update project
+	r.HandleFunc("/projects/delete/{id}", handlers.DeleteProject).Methods("DELETE") // Delete project
 
 	http.Handle("/", r)
 }
