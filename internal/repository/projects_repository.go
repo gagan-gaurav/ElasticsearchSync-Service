3d8748852fd706480101141bf78bf26693f7d488
Gagan Gaurav <iamgagangaurav@gmail.com>
2023-08-19 11:16:02 +0530

    added projects related database transactions.

--- internal/repository/projects_repository.go
+++ internal/repository/projects_repository.go
@@ -24,6 +24,21 @@ func GetProjectById(projectId int, project *models.Project) error {
 	return err
 }
 
+func GetProjectByIdForTransaction(tx *sql.Tx, projectId int, project *models.Project) error {
+	err := tx.QueryRow("SELECT * FROM projects WHERE id = $1", projectId).Scan(&project.ID, &project.Name, &project.Slug, &project.Description, &project.CreatedAt)
+	return err
+}
+
+func UpdateProject(tx *sql.Tx, project *models.Project) error {
+	_, err := tx.Exec("UPDATE projects SET name = $1, slug = $2, description = $3 WHERE id = $4", project.Name, project.Slug, project.Description, project.ID)
+	return err
+}
+
+func DeleteProject(tx *sql.Tx, projectId int) error {
+	_, err := tx.Exec("DELETE FROM projects WHERE id = $1", projectId)
+	return err
+}
+
 func ProjectExists(projectId int) bool {
 	var exists bool
 	err := database.DB.QueryRow("SELECT EXISTS(SELECT 1 FROM projects WHERE id = $1)", projectId).Scan(&exists)
@@ -81,7 +96,6 @@ func ProjectCreationAndSyncTransaction(project *models.Project) error {
 	// Insert project into the database
 	projectId, err := CreateProject(tx, project)
 	if err != nil {
-		fmt.Println(err)
 		tx.Rollback()
 		return err
 	}
@@ -90,54 +104,168 @@ func ProjectCreationAndSyncTransaction(project *models.Project) error {
 	users := project.UserIds
 
 	for _, userId := range users {
-		err = CreateUserProjects(tx, projectId, userId)
+		err = CreateProjectUsers(tx, projectId, userId)
 		if err != nil {
-			fmt.Println(err)
 			tx.Rollback()
 			return err
 		}
 	}
 
 	// Create entries in project_hashtags.
-
 	hashtags := project.HashtagIds
 
 	for _, hashtagId := range hashtags {
 		err = CreateProjectHashtags(tx, hashtagId, projectId)
 		if err != nil {
-			fmt.Println(err)
 			tx.Rollback()
 			return err
 		}
 	}
 
-	// Perform Denormalization of project and send it to sqs queue.
-
-	doc := createDoc(projectId, project)
-
-	err = GetProjectUsers(tx, projectId, &doc)
+	// Sync ElasticSearch
+	err = SyncElasticsearch(tx, projectId, "POST")
 	if err != nil {
-		fmt.Println(err)
+		tx.Rollback()
+		return err
+	}
+	// Commit the transaction
+	return tx.Commit()
+}
+
+func ProjectUpdateAndSyncTransaction(project *models.Project) error {
+	tx, err := database.DB.Begin()
+	if err != nil {
+		return err
+	}
+
+	// Update project into the database
+	err = UpdateProject(tx, project)
+	if err != nil {
 		tx.Rollback()
 		return err
 	}
 
-	err = GetProjectHashtags(tx, projectId, &doc)
+	//Remove old entries in user_projects.
+	err = DeleteProjectUsers(tx, project.ID)
 	if err != nil {
-		fmt.Println(err)
 		tx.Rollback()
 		return err
 	}
 
-	err = services.SQS(&doc)
+	// Update entries in user_projects.
+	users := project.UserIds
+
+	for _, userId := range users {
+		err = CreateProjectUsers(tx, project.ID, userId)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	//Remove old entries in project_hastags.
+	err = DeleteProjectHashtags(tx, project.ID)
 	if err != nil {
-		fmt.Println("SQS push failed")
+		tx.Rollback()
+		return err
+	}
+
+	// Update entries in project_hashtags.
+	hashtags := project.HashtagIds
+
+	for _, hashtagId := range hashtags {
+		err = CreateProjectHashtags(tx, hashtagId, project.ID)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	// Sync ElasticSearch
+	err = SyncElasticsearch(tx, project.ID, "POST")
+	if err != nil {
 		tx.Rollback()
 		return err
 	}
 
 	// Commit the transaction
 	return tx.Commit()
+}
+
+func ProjectDeleteAndSyncTransaction(projectId int) error {
+	tx, err := database.DB.Begin()
+	if err != nil {
+		return err
+	}
+
+	//Remove entries in user_projects.
+	err = DeleteProjectUsers(tx, projectId)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	//Remove entries in project_hastags.
+	err = DeleteProjectHashtags(tx, projectId)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// Delete document from elasticsearch
+	err = SyncElasticsearch(tx, projectId, "DELETE")
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// Delete project into the database
+	err = DeleteProject(tx, projectId)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// Commit the transaction
+	return tx.Commit()
+}
+
+func SyncElasticsearch(tx *sql.Tx, projectId int, method string) error {
+	// Perform Denormalization of project and send it to sqs queue.
+	var project models.Project
+	err := GetProjectByIdForTransaction(tx, projectId, &project)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	doc := createDoc(projectId, &project)
+
+	err = GetProjectUsers(tx, projectId, &doc)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = GetProjectHashtags(tx, projectId, &doc)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	//create payload
+	var payload models.Payload
+	payload.Doc = doc
+	payload.Method = method
+
+	err = services.SQS(&payload)
+	if err != nil {
+		fmt.Println("SQS push failed")
+		tx.Rollback()
+		return err
+	}
+
+	return err
 }
 
 func createDoc(projectId int, project *models.Project) models.DenormalizedProject {
