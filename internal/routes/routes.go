19413caabba32f297edb079ad5f2043e0c900c6b
Gagan Gaurav <iamgagangaurav@gmail.com>
2023-08-20 00:46:01 +0530

    get all routes added

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -14,14 +14,17 @@ func SetRouter() {
 	// Define routes
 	r.HandleFunc("/users", handlers.CreateUser).Methods("POST")                     // Create user
 	r.HandleFunc("/users/{id}", handlers.GetUser).Methods("GET")                    // Get user
+	r.HandleFunc("/users", handlers.GetAllUsers).Methods("GET")                     // Get all users
 	r.HandleFunc("/users/update/{id}", handlers.UpdateUser).Methods("POST")         // Update user
 	r.HandleFunc("/users/delete/{id}", handlers.DeleteUser).Methods("DELETE")       // Delete user
 	r.HandleFunc("/hashtags", handlers.CreateHashtag).Methods("POST")               // Create hashtag
 	r.HandleFunc("/hashtags/{id}", handlers.GetHashtag).Methods("GET")              // Get hashtag
+	r.HandleFunc("/hashtags", handlers.GetAllHashtags).Methods("GET")               // Get all hashtags
 	r.HandleFunc("/hashtags/update/{id}", handlers.UpdateHashtag).Methods("POST")   // Update hashtag
 	r.HandleFunc("/hashtags/delete/{id}", handlers.DeleteHashtag).Methods("DELETE") // Delete hastag
 	r.HandleFunc("/projects", handlers.CreateProject).Methods("POST")               // Create project
 	r.HandleFunc("/projects/{id}", handlers.GetProject).Methods("GET")              // Get project
+	r.HandleFunc("/projects", handlers.GetAllProjects).Methods("GET")               // Get all projects
 	r.HandleFunc("/projects/update/{id}", handlers.UpdateProject).Methods("POST")   // Update project
 	r.HandleFunc("/projects/delete/{id}", handlers.DeleteProject).Methods("DELETE") // Delete project
 
